753b341876bcd2fcd46e512fbdcff587295266d3
sergiobernalparrondo <serberpar@gmail.com>
2021-11-26 20:17:47 +0100

    added more Primitives

--- src/main/java/io/github/divios/core_lib/utils/Primitives.java
+++ src/main/java/io/github/divios/core_lib/utils/Primitives.java
@@ -3,8 +3,26 @@ package io.github.divios.core_lib.utils;
 public class Primitives {
 
     public static boolean isInteger(String s) {
+        return testCast(() -> Integer.parseInt(s));
+    }
+
+    public static int getAsInteger(String s) {
+        if (!isInteger(s)) throw new RuntimeException("String passed is not an integer");
+        return Integer.parseInt(s);
+    }
+
+    public static boolean isBoolean(String s) {
+        return testCast(() -> Boolean.getBoolean(s));
+    }
+
+    public static boolean getAsBoolean(String s) {
+        if (isBoolean(s)) throw new RuntimeException("String passed is not a boolean");
+        return Boolean.getBoolean(s);
+    }
+
+    private static boolean testCast(Runnable runnable) {
         try {
-            Integer.parseInt(s);
+            runnable.run();
         } catch (Exception e) {
             return false;
         }
