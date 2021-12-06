251a6f20509e92c3a40eacbf920f6aeab77de39d
sergiobernalparrondo <serberpar@gmail.com>
2021-12-06 13:29:35 +0100

    Fixed Base64Utils

--- src/main/java/io/github/divios/core_lib/serialize/Base64Utils.java
+++ src/main/java/io/github/divios/core_lib/serialize/Base64Utils.java
@@ -6,11 +6,19 @@ import java.util.Base64;
 
 public class Base64Utils {
 
+    public static String encode(String buf) {
+        return encode(buf.getBytes());
+    }
+
     public static String encode(byte[] buf) {
         return Base64.getEncoder().encodeToString(buf);
     }
 
-    public static byte[] decode(String src) {
+    public static String decode(String src) {
+        return new String(decodeBytes(src));
+    }
+
+    private static byte[] decodeBytes(String src) {
         try {
             return Base64.getDecoder().decode(src);
         } catch (IllegalArgumentException e) {
